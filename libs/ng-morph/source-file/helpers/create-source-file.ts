448b1836b62f60ab46cb54f836f27571379819d3
IKatsuba <katsuba.igor@gmail.com>
2021-06-09 14:21:06 +0300

    feat: add the `overwrite` option in createSourceFile

--- libs/ng-morph/source-file/helpers/create-source-file.ts
+++ libs/ng-morph/source-file/helpers/create-source-file.ts
@@ -3,7 +3,8 @@ import { SourceFile } from 'ts-morph';
 
 export function createSourceFile(
   filePath: string,
-  content?: string
+  content?: string,
+  { overwrite = false }: { overwrite?: boolean } = {}
 ): SourceFile {
-  return getActiveProject().createSourceFile(filePath, content);
+  return getActiveProject().createSourceFile(filePath, content, { overwrite });
 }
