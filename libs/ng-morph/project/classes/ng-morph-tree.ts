83d83accb6ac78b63094a9a7567ef5e9cf4148b2
IKatsuba <katsuba.igor@gmail.com>
2021-06-09 11:14:02 +0300

    feat: added a tree to run ng-morph without schematics

---
+++ libs/ng-morph/project/classes/ng-morph-tree.ts
@@ -0,0 +1,10 @@
+import { HostTree } from '@angular-devkit/schematics';
+import { ScopedHost } from '@angular-devkit/core/src/virtual-fs/host';
+import { NodeJsSyncHost } from '@angular-devkit/core/node';
+import { normalize } from '@angular-devkit/core';
+
+export class NgMorphTree extends HostTree {
+  constructor(root: string = __dirname) {
+    super(new ScopedHost(new NodeJsSyncHost(), normalize(root)));
+  }
+}
