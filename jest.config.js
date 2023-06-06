eda96f2e94411469f410edd2a884fdff8f544de4
Alex Skoropad <alex@skoropad.dev>
2023-06-06 17:04:03 +0200

    chore(project): migrate to nx@13

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,3 @@
-module.exports = {
-  projects: ['<rootDir>/libs/ng-morph', '<rootDir>/apps/demo'],
-};
+const { getJestProjects } = require('@nrwl/jest');
+
+module.exports = { projects: getJestProjects() };
