981a7b5d2452f8bae2032bd9e111f84675c6dab7
Alex Skoropad <alex@skoropad.dev>
2023-07-01 18:51:45 +0200

    chore(project): update dependencies

--- apps/demo/src/app/app.module.ts
+++ apps/demo/src/app/app.module.ts
@@ -1,20 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {
-  TuiDocMainModule,
-  TUI_DOC_LOGO,
-  TUI_DOC_PAGES, TUI_DOC_EXAMPLE_MARKDOWN_CODE_PROCESSOR,
-} from '@taiga-ui/addon-doc';
-import { TuiLinkModule } from '@taiga-ui/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {TUI_DOC_LOGO, TUI_DOC_PAGES, TuiDocMainModule,} from '@taiga-ui/addon-doc';
+import {TuiLinkModule} from '@taiga-ui/core';
 
-import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-import { LOGO_CONTENT } from './logo/logo.component';
-import { pages } from './pages';
-import { GettingStartedModule } from './getting-started/getting-started.module';
-import { ROUTES } from './routes';
-import { HIGHLIGHT_OPTIONS } from 'ngx-highlightjs';
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import {LOGO_CONTENT} from './logo/logo.component';
+import {pages} from './pages';
+import {GettingStartedModule} from './getting-started/getting-started.module';
+import {ROUTES} from './routes';
+import {HIGHLIGHT_OPTIONS} from 'ngx-highlightjs';
 
 export const HIGHLIGHT_OPTIONS_VALUE = {
   coreLibraryLoader: () => import('highlight.js/lib/core'),
