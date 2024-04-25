4732b18ef19ec29452c2231eaa1841110df3d654
splincode <omaxphp@yandex.ru>
2024-04-25 11:16:09 +0300

    feat: add `isStandaloneComponent`

---
+++ libs/ng-morph/ng/helpers/ng-component.ts
@@ -0,0 +1,13 @@
+import type {ClassDeclaration} from 'ts-morph';
+
+import {getClasses} from '../../classes';
+import type {Pattern, Query, StructureType} from '../../utils';
+
+export function getNgComponents(
+    pattern: Pattern,
+    query?: Query<Omit<StructureType<ClassDeclaration>, 'kind'>>,
+): ClassDeclaration[] {
+    return getClasses(pattern, query).filter(
+        declaration => !!declaration.getDecorator('Component'),
+    );
+}
--- libs/ng-morph/ng/index.ts
+++ libs/ng-morph/ng/index.ts
@@ -2,4 +2,7 @@ export * from './bootstrap';
 export * from './component';
 export * from './directive';
 export * from './helpers';
+export * from './helpers/is-standalone-component';
+export * from './helpers/ng-component';
+export * from './helpers/ng-module';
 export * from './module';
