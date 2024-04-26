67a4dfc82e87df46362feecaaa576ca203348fc7
splincode <omaxphp@yandex.ru>
2024-04-26 10:16:28 +0300

    fix: invalid helpers entry point

--- libs/ng-morph/ng/helpers/index.ts
+++ libs/ng-morph/ng/helpers/index.ts
@@ -1,3 +1,6 @@
 export * from './get-main-module';
+export * from './is-standalone-component';
+export * from './ng-component';
+export * from './ng-module';
 export * from './push-to-decorator-array-property';
 export * from './push-to-object-literal-array-property';
