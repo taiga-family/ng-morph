513783bf60da8ed705c317151e8dd3ff1b7f088b
null <taiga-family-bot@yandex.ru>
2024-10-07 23:26:22 +0000

    chore(deps): update taiga-ui family dependencies to v4

--- apps/demo/src/app/app.component.ts
+++ apps/demo/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {TuiDocMainModule} from '@taiga-ui/addon-doc';
-import {TuiLinkModule} from '@taiga-ui/core';
+import {TuiAddonDoc} from '@taiga-ui/addon-doc';
+import {TuiLink} from '@taiga-ui/core';
 import {HighlightModule} from 'ngx-highlightjs';
 
 @Component({
     standalone: true,
     selector: 'app-root',
-    imports: [HighlightModule, TuiDocMainModule, TuiLinkModule],
+    imports: [HighlightModule, TuiAddonDoc, TuiLink],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.less'],
     changeDetection: ChangeDetectionStrategy.OnPush,
