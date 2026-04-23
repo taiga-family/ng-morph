31c1921f15bb7bc0745830ee738bbb444f529895
splincode <omaxphp@yandex.ru>
2026-04-23 12:57:21 +0300

    feat: support filter pattern for tree

--- libs/ng-morph/src/project/ng-morph-tree.ts
+++ libs/ng-morph/src/project/ng-morph-tree.ts
@@ -1,16 +1,30 @@
 import {normalize} from '@angular-devkit/core';
 import {NodeJsSyncHost} from '@angular-devkit/core/node';
 import {ScopedHost} from '@angular-devkit/core/src/virtual-fs/host/scoped';
-import {HostSink, HostTree} from '@angular-devkit/schematics';
+import {FilterHostTree, HostSink, HostTree} from '@angular-devkit/schematics';
+import {minimatch} from 'minimatch';
 import {firstValueFrom} from 'rxjs';
 
-export class NgMorphTree extends HostTree {
+const DEFAULT_IGNORE_PATTERNS = [
+    '**/node_modules/**',
+    '**/.git/**',
+    '**/.nx/**',
+    '**/dist/**',
+    '**/build/**',
+    '**/.cache/**',
+    '**/tmp/**',
+    '**/temp/**',
+    '**/*.log',
+];
+
+export class NgMorphTree extends FilterHostTree {
     private readonly hostSink: HostSink;
 
-    constructor(root: string = process.cwd()) {
+    constructor(root: string = process.cwd(), ignores = DEFAULT_IGNORE_PATTERNS) {
         const host = new ScopedHost(new NodeJsSyncHost(), normalize(root));
+        const tree = new HostTree(host);
 
-        super(host);
+        super(tree, (path) => !ignores.some((pattern) => minimatch(path, pattern)));
 
         this.hostSink = new HostSink(host);
     }
--- libs/ng-morph/tests/ng-morph-tree.spec.ts
+++ libs/ng-morph/tests/ng-morph-tree.spec.ts
@@ -32,16 +32,30 @@ describe('NgMorphTree', () => {
             .toBe(`import {normalize} from '@angular-devkit/core';
 import {NodeJsSyncHost} from '@angular-devkit/core/node';
 import {ScopedHost} from '@angular-devkit/core/src/virtual-fs/host/scoped';
-import {HostSink, HostTree} from '@angular-devkit/schematics';
+import {FilterHostTree, HostSink, HostTree} from '@angular-devkit/schematics';
+import {minimatch} from 'minimatch';
 import {firstValueFrom} from 'rxjs';
 
-export class NgMorphTree extends HostTree {
+const DEFAULT_IGNORE_PATTERNS = [
+    '**/node_modules/**',
+    '**/.git/**',
+    '**/.nx/**',
+    '**/dist/**',
+    '**/build/**',
+    '**/.cache/**',
+    '**/tmp/**',
+    '**/temp/**',
+    '**/*.log',
+];
+
+export class NgMorphTree extends FilterHostTree {
     private readonly hostSink: HostSink;
 
-    constructor(root: string = process.cwd()) {
+    constructor(root: string = process.cwd(), ignores = DEFAULT_IGNORE_PATTERNS) {
         const host = new ScopedHost(new NodeJsSyncHost(), normalize(root));
+        const tree = new HostTree(host);
 
-        super(host);
+        super(tree, (path) => !ignores.some((pattern) => minimatch(path, pattern)));
 
         this.hostSink = new HostSink(host);
     }
@@ -68,5 +82,43 @@ export class NgMorphTree extends HostTree {
         );
 
         spy.mockRestore();
+    });
+
+    it('should filter out files matching default ignore patterns', () => {
+        const treeWithIgnores = new NgMorphTree(dirname);
+
+        const allFiles: string[] = [];
+
+        treeWithIgnores.visit((path) => {
+            allFiles.push(path.toString());
+        });
+
+        allFiles.forEach((file) => {
+            expect(file).not.toMatch(/node_modules/);
+            expect(file).not.toMatch(/\.git/);
+            expect(file).not.toMatch(/\.nx/);
+            expect(file).not.toMatch(/\.log$/);
+        });
+    });
+
+    it('should filter out files matching custom ignore patterns', () => {
+        const customIgnorePatterns = ['**/*.spec.ts', '**/tests/**'];
+        const treeWithCustomIgnores = new NgMorphTree(dirname, customIgnorePatterns);
+
+        const allFiles: string[] = [];
+
+        treeWithCustomIgnores.visit((path) => {
+            allFiles.push(path.toString());
+        });
+
+        allFiles.forEach((file) => {
+            expect(file).not.toMatch(/\.spec\.ts$/);
+        });
+    });
+
+    it('should allow empty ignore patterns', () => {
+        const treeWithoutIgnores = new NgMorphTree(dirname, []);
+
+        expect(treeWithoutIgnores).toBeInstanceOf(NgMorphTree);
     });
 });
