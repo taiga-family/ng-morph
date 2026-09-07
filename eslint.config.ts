94ba645252fd394cb7208fb36dc9e4b4f407482d
Максим Иванов <omaxphp@yandex.ru>
2026-09-07 11:30:10 +0300

    chore: add Jest ESLint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -2,6 +2,7 @@ import taiga from '@taiga-ui/eslint-plugin-experience-next';
 
 export default [
     ...taiga.configs.recommended,
+    ...taiga.configs.jest,
     {
         files: ['**/*.ts'],
         rules: {'import/no-cycle': 'off'},
